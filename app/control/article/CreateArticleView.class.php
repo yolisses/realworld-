03133e5ee5a06520d6b93bf8cfc5bfa837891948
yolisses <iamurise@gmail.com>
2024-07-19 12:35:24 -0300

    create article edit

--- app/control/article/CreateArticleView.class.php
+++ app/control/article/CreateArticleView.class.php
@@ -2,9 +2,9 @@
 
 use Adianti\Control\TAction;
 use Adianti\Control\TPage;
-use Adianti\Core\AdiantiCoreApplication;
 use Adianti\Database\TTransaction;
 use Adianti\Widget\Form\TEntry;
+use Adianti\Widget\Form\THidden;
 use Adianti\Widget\Form\TLabel;
 use Adianti\Widget\Form\TText;
 use Adianti\Wrapper\BootstrapFormBuilder;
@@ -16,7 +16,7 @@ class CreateArticleView extends TPage
 
   use Adianti\Base\AdiantiStandardFormTrait;
 
-  public function __construct()
+  function __construct()
   {
     parent::__construct();
 
@@ -29,13 +29,23 @@ class CreateArticleView extends TPage
     $title = new TEntry('title');
     $description = new TEntry('description');
     $body = new TText('body');
+    $id = new THidden('id');
 
     $this->form->addFields([new TLabel("Title")], [$title]);
     $this->form->addFields([new TLabel("Description")], [$description]);
     $this->form->addFields([new TLabel('Body')], [$body]);
-
+    $this->form->addFields([$id]);
     $this->form->addAction("Publish article", new TAction(array($this, 'onSave')), '');
 
     $this->add($this->form);
+  }
+
+  function onLoad($params)
+  {
+    if (!$params['id']) return;
+    TTransaction::open('sample');
+    $article = new Article($params['id']);
+    $this->form->setData($article);
+    TTransaction::close();
   }
 }
