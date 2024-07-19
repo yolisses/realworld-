73299cbd0328d5f584c2eafa1f2f9501b525746b
yolisses <iamurise@gmail.com>
2024-07-19 08:41:15 -0300

    create sign up form

--- app/control/user/SignUpView.class.php
+++ app/control/user/SignUpView.class.php
@@ -2,14 +2,38 @@
 
 use Adianti\Control\TAction;
 use Adianti\Control\TPage;
+use Adianti\Widget\Form\TEntry;
+use Adianti\Widget\Form\TLabel;
+use Adianti\Widget\Form\TPassword;
 use Adianti\Widget\Wrapper\TQuickForm;
+use Adianti\Wrapper\BootstrapFormBuilder;
 use Adianti\Wrapper\BootstrapFormWrapper;
 
 class SignUpView extends TPage
 {
   public function __construct()
   {
+
     parent::__construct();
-    echo '<h1>Sign Up</h1>';
+
+
+    $username = new TEntry('username');
+    $email = new TEntry("email");
+    $password = new TPassword("password");
+
+    $form = new BootstrapFormBuilder;
+    $this->add($form);
+    $form->setFormTitle('Sign Up');
+
+    $form->addFields([new TLabel("Username")], [$username]);
+    $form->addFields([new TLabel("Email")], [$email]);
+    $form->addFields([new TLabel("Password")], [$password]);
+
+    $form->addAction("Sign up", new TAction(array($this, 'onSend')));
+  }
+
+  public function onSend()
+  {
+    echo "here";
   }
 }
